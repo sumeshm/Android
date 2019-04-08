19b167073e3b6326ff80f3ba50e1b9d075a5c6ac
smani5 <smani5@intuit.com>
2019-04-08 17:10:16 +0530

    Creating quick calendar widget

---
+++ QuickCalendarWidget/app/src/main/java/planetjup/com/widget/MainActivity.java
@@ -0,0 +1,15 @@
+package planetjup.com.widget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import planetjup.com.widget.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
